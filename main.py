2246d088f34c986ea2fdccee9cb175bec62b084d
goulart <goularto2020@gmail.com>
2025-10-17 22:03:26 -0300

    Tarefas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #importação de bibliotecas
 from flask import Flask 
+from tarefa import buscar_tarefas
 
 #cria o objeto do flask
 app = Flask(__name__)
@@ -8,6 +9,12 @@ app = Flask(__name__)
 @app.route('/api')
 def index():
     return 'Api rodando'
+
+#criando a rota que retorna as tarefas
+@app.route ('/api/tarefas')   
+def get_tarefas():
+    tarefas = buscar_tarefas()
+    return tarefas
 
 #identifica que é o arquivo principal
 #e liga o servidor executando o Flask 🤣
---
+++ tarefa.py
@@ -0,0 +1,12 @@
+def buscar_tarefas():
+    tarefas = [{
+        "id": 1,
+        "tarefa": "Aprender python",
+        "status": "Pendente",
+        },
+        {
+        "id": 2,
+        "tarefa": "Aprender HTML",
+        "status": "Concluido",
+        }
+    ]
