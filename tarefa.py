b1d5f7cec500e8b8f5d8980aaf6f93832cea2b1f
goulart <goularto2020@gmail.com>
2025-11-07 20:25:22 -0300

    bd

--- tarefa.py
+++ tarefa.py
@@ -1,13 +1,25 @@
+#importa a conexao criada
+from conexao import get_conexao
+
+#importa a bibilioteca do psycog2
+from psycopg2.extras import RealDictCursor
+
+#importa o jsonfy do flask para
+#retornar os dados no formato json 
+from flask import jsonify
+
 def buscar_tarefas():
-    tarefas = [{
-        "id": 1,
-        "tarefa": "Aprender python",
-        "status": "Pendente",
-        },
-        {
-        "id": 2,
-        "tarefa": "Aprender HTML",
-        "status": "Concluido",
-        }
-    ]
-    return tarefas
+    conn = get_conexao()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute(
+        "SELECT id, nome, descricao FROM tarefas;"
+    )
+    #Busca todos registros na tabela
+    tarefas = cursor.fetchall()
+
+    #Fecha as conexoes 
+    cursor.close()
+    conn.close()
+
+    return jsonify(tarefas)
+   
