6081e78a2c530f587a06ce2c5ec02f805a56dfbd
goulart <goularto2020@gmail.com>
2025-11-07 21:44:05 -0300

    env

--- conexao.py
+++ conexao.py
@@ -1,13 +1,20 @@
+#importa bibiliotecas
 import psycopg2
+from dotenv import load_dotenv
+#importa o modulo OS do python
+import os
+
+#carrega o arquivo env
+load_dotenv()
 
 # Funcao que retorna a conexao
 # com banco de dados postgres
 def get_conexao():
     conn = psycopg2.connect(
-        dbname='todolist',
-        user='postgres',
-        password='postgres',
-        host='127.0.0.1',
-        port=5432
+        dbname=os.getenv('DB_DATABASE'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT')
     )
     return conn
